bbe8ee5ec8540e7a40b6d86635bd5e69931c69cc
RayamVenkataDivya <rayam162002@gmail.com>
2024-02-29 11:57:27 +0530

    werty

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios'
 
 function App() {
+  var [countries,setCountries]=React.useState([])
+  var [loader,setLoader]=React.useState(true)
+
+  React.useEffect(()=>{
+    axios.get("https://restcountries.com/v3/all")
+    .then((res)=>{
+      setCountries(res.data);
+      setLoader(false)
+    })
+    .catch(()=>{})
+  },[])
+
   return (
     <div className="App">
-      <h1>Welcome to React Application</h1>
- 
+      <h1>Countries</h1>
+      {loader && <h3>Loading....</h3>}
+      <ul>
+        {
+          countries.map((c)=>{
+            return <li>{c.name.common}</li> 
+          })
+        }
+      </ul>
+      
     </div>
   );
 }
