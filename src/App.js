811c6281f3e964c175fb660f4cc234d9e309bf09
RayamVenkataDivya <rayam162002@gmail.com>
2024-03-06 15:52:08 +0530

    gfg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  var [students,setStudents]=React.useState([
+    {
+      fname:"Divya",
+      lname:"Rayam",
+      gender:"Female"
+    },
+    {
+      fname:"Rakesk",
+      lname:"Rayam",
+      gender:"Male"
+    },
+    {
+      fname:"Sirisha",
+      lname:"Annavarapu",
+      gender:"Female"
+    },
+    {
+      fname:"Dhanu",
+      lname:"Aitha",
+      gender:"Male"
+    },
+    {
+      fname:"Diyaa",
+      lname:"Chowdary",
+      gender:"Female"
+    },
+    {
+      fname:"Sakshi",
+      lname:"Reddy",
+      gender:"Female"
+    },
+    {
+      fname:"Deepak",
+      lname:"Aitha",
+      gender:"Male"
+    }
+
+  ])
   return (
-    <div className="App">
-      <h1>Welcome to React Application</h1>
+    <div style={{border:"2px solid red",padding:"10px",margin:"10px"}}>
+      <table style={{border:"1px solid black",marginLeft:"40%"}}>
+        <thead>
+          <tr style={{border:"1px solid black"}} >
+            <th style={{border:"1px solid black"}}>Firstname</th>
+            <th style={{border:"1px solid black"}}>Lastname</th>
+            <th style={{border:"1px solid black"}}>Gender</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            students.map((student)=>{
+              return (
+                <tr style={student.gender==="Female"?{backgroundColor:"pink"}:{backgroundColor:"cyan"}}>
+                  <td style={{border:"1px solid black"}} >{student.fname}</td>
+                  <td style={{border:"1px solid black"}}>{student.lname}</td>
+                  <td style={{border:"1px solid black"}}>{student.gender}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
  
     </div>
   );
