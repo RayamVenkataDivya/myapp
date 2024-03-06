9d06fe8b1427c8ebad711688401a4e9289e372ae
RayamVenkataDivya <rayam162002@gmail.com>
2024-03-06 12:28:38 +0530

    sdf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  var [students,setStudents]=React.useState(["divya","sirisha","harshitha","bharathi"])
   return (
-    <div className="App">
-      <h1>Welcome to React Application</h1>
+    <div className="container" style={{boxShadow:"1px 10px 10px 2px",width:"20%",border:"2px solid rgb(132, 119, 119)",padding:"10px",margin:"10px auto"}}>
+      <h1>Student</h1>
+      {
+        students.map((s)=>{
+          return <li>{s}</li>
+        })
+      }
  
     </div>
   );
