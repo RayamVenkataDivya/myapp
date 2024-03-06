a9d1d9e522e4602a277fb34461464dbcf32a1588
RayamVenkataDivya <rayam162002@gmail.com>
2024-03-06 16:36:46 +0530

    wde

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  var [count,setCount]=React.useState(10)
+  function incBy10(){
+    setCount(count+10)
+  }
+  function incBy100(){
+    setCount(count+100)
+  }
+  function decBy10(){
+    setCount(count-10)
+  }
   return (
     <div className="App">
-      <h1>Welcome to React Application</h1>
- 
+      <h3>Welcome to ReactJs Counter</h3>
+      <h1>{count}</h1>
+      <button onClick={incBy10}>Inc count by 10</button>
+      <button onClick={incBy100}>Inc count by 100</button>
+      <button onClick={decBy10}>Dec count by 10</button>
     </div>
   );
 }
