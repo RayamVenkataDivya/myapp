150e324eb5e6a4e1f03e284f450785b4b7e25698
RayamVenkataDivya <rayam162002@gmail.com>
2024-03-06 15:28:49 +0530

    dfgh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  var [courseDetails,setCourseDetails]=React.useState([
+    {
+    cname:"React",
+    ctrainer:"Praveen Gubbala",
+    ccost:25000
+    },
+    {
+      cname:"Angular",
+      ctrainer:"Dritiman",
+      ccost:30000
+    },
+    {
+      cname:"Java",
+      ctrainer:"Ramesh",
+      ccost:35000
+    },
+    {
+      cname:"React",
+      ctrainer:"Praveen Gubbala",
+      ccost:25000
+    }
+  ])
   return (
-    <div className="App">
-      <h1>Welcome to React Application</h1>
- 
+    <div  style={{border:'1px solid',padding:'10px',margin:'10px'}}>
+      {
+        courseDetails.map((course,i)=>{
+          return (
+            <div  style={{border:'1px solid',padding:'2px',margin:'3px'}}>
+              <h3>CourseName:{course.cname}</h3>
+              <h3>CourseTrainer:{course.ctrainer}</h3>
+              <h3>CourseCost:{course.ccost}</h3>
+            </div>
+          )
+        })
+      }
     </div>
-  );
+   
+  )
 }
 
 export default App;
