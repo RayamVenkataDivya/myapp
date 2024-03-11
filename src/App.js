a1edc6ce025486f1a3efef7e4f11ebe21c1e4b84
RayamVenkataDivya <rayam162002@gmail.com>
2024-03-11 16:59:20 +0530

    nb

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  var [students,setStudents]=React.useState(["divya","kavya","bhanu","sai"])
+  var [newStudent,setNewStudent]=React.useState()
+
+  function add(){
+    var temp=[...students]
+    temp.push(newStudent)
+    setStudents(temp)
+  }
+  
+  function asce(){
+    var temp=[...students]
+    temp.sort((a,b)=>{
+      if(a<b){
+        return -1;
+      }
+      if(a>b){
+        return 1;
+      }
+      return 0;
+    })
+    setStudents(temp)
+  }
+
+  function desc(){
+    students.sort((a,b)=>{
+      if(a<b){
+        return 1;
+      }
+      if(a>b){
+        return -1
+      }
+      return 0;
+    })
+    setStudents([...students])
+  }
   return (
-    <div className="App">
-      <h1>Welcome to React Application</h1>
+    <div>
+      <h1>React Students</h1>
+      <input type="text" onKeyUp={(e)=>{setNewStudent(e.target.value)}}/>
+      <button onClick={add}>Add Students</button>
+      <button onClick={asce}>Ascending Order</button>
+      <button onClick={desc}>Descending Order</button>
+      <ul>
+        {
+          students.map((student)=>{
+            return <li>{student}</li>
+          })
+        }
+      </ul>
  
     </div>
   );
 }
 
 export default App;
+
