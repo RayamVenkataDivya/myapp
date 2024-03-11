286fb42de6e55cab7bc1f2f350f6f7138e323b81
RayamVenkataDivya <rayam162002@gmail.com>
2024-03-11 16:08:27 +0530

    gfds

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  var [result,setResult]=React.useState()
+
+  function add(){
+    var n1=+document.getElementById("d1").value
+    var n2=+document.getElementById("d2").value
+    setResult(n1+n2)
+  }
+  
+  function sub(){
+    var n1=+document.getElementById("d1").value
+    var n2=+document.getElementById("d2").value
+    setResult(n1-n2)
+  }
+  
+  function mul(){
+    var n1=+document.getElementById("d1").value
+    var n2=+document.getElementById("d2").value
+    setResult(n1*n2)
+  }
+  
+  function div(){
+    var n1=+document.getElementById("d1").value
+    var n2=+document.getElementById("d2").value
+    setResult(n1/n2)
+  }
   return (
-    <div className="App">
-      <h1>Welcome to React Application</h1>
- 
+    <div>
+      <input type="text" id="d1"/>
+      <input type="text" id="d2"/>
+      <button onClick={add}>Addition</button>
+      <button onClick={sub}>Substraction</button>
+      <button onClick={mul}>Multiplication</button>
+      <button onClick={div}>Division</button>
+      <h1>{result}</h1>
     </div>
   );
 }
