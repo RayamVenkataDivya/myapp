c49010a91e5e1fb372a6b888c7085dbb2e6a4719
RayamVenkataDivya <rayam162002@gmail.com>
2024-03-16 11:00:17 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>React Cricket Application</h1>
+      <h1>Welcome to Edupoly.........</h1>
  
     </div>
   );
